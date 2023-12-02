71b37a664b3d424bb6f38a810131a6114222cc89
r-q <scrisqiu@hotmail.com>
2023-12-02 22:55:18 +0800

    make basic cli

---
+++ Encoder/Wav2pcm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Roderick Qiu on 2023/12/2.
+//
+
+#include "Wav2pcm.h"
---
+++ Encoder/Wav2pcm.h
@@ -0,0 +1,20 @@
+//
+// Created by Roderick Qiu on 2023/12/2.
+//
+
+#ifndef GTWD_WAV2PCM_H
+#define GTWD_WAV2PCM_H
+
+#include<string>
+
+using namespace std;
+
+class Wav2pcm {
+public:
+    static string hello() {
+        return "Hello from Wav2pcm";
+    }
+};
+
+
+#endif //GTWD_WAV2PCM_H
